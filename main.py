67b09941a709e5795ce933492794e4180de51229
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-02-27 09:38:54 +0200

    fix: the web app is working

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
-from flask_bootstrap import Bootstrap5
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
@@ -23,7 +23,7 @@ This will install the packages from requirements.txt for this project.
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-Bootstrap5(app)
+Bootstrap(app)
 
 # CREATE DB
 
@@ -32,18 +32,42 @@ class Base(DeclarativeBase):
 
 db = SQLAlchemy(model_class=Base)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///top_movies.db'
 
 db.init_app(app)
 
 # CREATE TABLE
 
 class Movie(db.Model):
-    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(db.String, unique=True, nullable=False)
-    film_director: Mapped[str] = mapped_column(db.String, nullable=False)
-    release_year: Mapped[int] = mapped_column(db.Integer, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String, unique=False, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String)
+    rating: Mapped[float] = mapped_column(Float, default = 0)
+    ranking: Mapped[int] = mapped_column(Integer, default=0)
+    review: Mapped[str] = mapped_column(String)
+    img_url: Mapped[str] = mapped_column(String, nullable=False)
 
+with app.app_context():
+    db.create_all()
+
+
+
+with app.app_context():
+    new_movie = Movie(
+        title="Phone Booth",
+        year=2002,
+        description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's "
+                    "sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to"
+                    " a jaw-dropping climax.",
+        rating=7.3,
+        ranking=10,
+        review="My favourite character was the caller.",
+        img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+    )
+
+    db.session.add(new_movie)
+    db.session.commit()
 
 @app.route("/")
 def home():
