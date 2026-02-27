df03b974f264e4451796e8ddcf3d1d93ad8e5d1d
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-02-27 14:43:25 +0200

    fix: remove the intial insert of the movie right after the table was created

--- main.py
+++ main.py
@@ -57,21 +57,21 @@ with app.app_context():
 
 # Add the first entry into database
 
-with app.app_context():
-    new_movie = Movie(
-        title="Phone Booth",
-        year=2002,
-        description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's "
-                    "sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to"
-                    " a jaw-dropping climax.",
-        rating=7.3,
-        ranking=10,
-        review="My favourite character was the caller.",
-        img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-    )
-
-    db.session.add(new_movie)
-    db.session.commit()
+# with app.app_context():
+#     new_movie = Movie(
+#         title="Phone Booth",
+#         year=2002,
+#         description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's "
+#                     "sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to"
+#                     " a jaw-dropping climax.",
+#         rating=7.3,
+#         ranking=10,
+#         review="My favourite character was the caller.",
+#         img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+#     )
+#
+#     db.session.add(new_movie)
+#     db.session.commit()
 
 # Create Movie Edit Flask Form
 
